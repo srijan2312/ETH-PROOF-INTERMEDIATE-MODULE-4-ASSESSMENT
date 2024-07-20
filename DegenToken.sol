020ec661dbd5053b7bf845c52488de4d68ee9d9d
Srijan Kumar <132009370+srijan2312@users.noreply.github.com>
2024-07-20 20:48:19 +0530

    Update DegenToken.sol

--- DegenToken.sol
+++ DegenToken.sol
@@ -8,10 +8,13 @@ import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/v4.4.0/contr
 contract DegenToken is ERC20, Ownable, Pausable {
 
     // Mapping of items and their token cost
-    mapping(string => uint256) public itemCosts;
+    mapping(uint256 => uint256) public itemCosts;
 
     // Constructor to initialize the token with name and symbol
     constructor() ERC20("Degen", "DGN") {
+        itemCosts[1] = 150; // item 1 costs 150 tokens (150 DGN)
+        itemCosts[2] = 200; // item 2 costs 200 tokens (200 DGN)
+        itemCosts[3] = 250; // item 3 costs 250 tokens (250 DGN)
     }
 
     // Function to create new tokens and distribute them to players as rewards, can only be called by the owner
@@ -24,14 +27,9 @@ contract DegenToken is ERC20, Ownable, Pausable {
         _burn(msg.sender, amount);
     }
 
-    // Function to set the cost of items
-    function setItemCost(string memory itemName, uint256 cost) external onlyOwner {
-        itemCosts[itemName] = cost;
-    }
-
     // Function to redeem their tokens for items in the in-game store
-    function redeemItem(string memory itemName) external whenNotPaused {
-        uint256 itemCost = itemCosts[itemName];
+    function redeemItem(uint256 itemId) external whenNotPaused {
+        uint256 itemCost = itemCosts[itemId];
         require(itemCost > 0, "Item does not exist or cost not set");
         require(balanceOf(msg.sender) >= itemCost, "Insufficient tokens");
 
